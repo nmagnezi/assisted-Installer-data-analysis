e015994ebd3d73d9b4bd06d844db0f8f9215ff27
Nir Magnezi <nmagnezi@redhat.com>
2023-02-14 15:46:43 +0200

    Print a general breakdown of cluster states from gathered data

--- src/common/helpers.py
+++ src/common/helpers.py
@@ -5,6 +5,14 @@ LOG = logging.getLogger(__name__)
 
 from common import stats
 
+
+class Dict2Class(object):
+
+    def __init__(self, my_dict):
+        for key in my_dict:
+            setattr(self, key, my_dict[key])
+
+
 def configure_logger(debug_mode):
     logging.basicConfig(
         level=logging.DEBUG if debug_mode is True else logging.INFO,
@@ -18,13 +26,18 @@ def load_json_from_file(file):
     return json_object
 
 
+def _print_cluster_states(cluster_states):
+    for state, amount in cluster_states.items():
+        LOG.info(f"Cluster states: {amount} {state} ")
+
+
 def print_data_stats(clusters, infraenvs, hosts, env):
     data = stats.prepare_data(clusters, infraenvs, hosts)
-
     LOG.info(f"Facts about the data scraped from {env}")
     LOG.info(f"Total of {len(clusters)} clusters and {len(infraenvs)} infraenvs total")
-    LOG.info(f"Detected {data['count_unbound_infraenvs']} unbounded infraenvs")
-    LOG.info(f"Out of {len(infraenvs)} infraenvs, {data['infraenvs_with_static_network_config']} "
+    _print_cluster_states(data.cluster_states)
+    LOG.info(f"Detected {data.count_unbound_infraenvs} unbounded infraenvs")
+    LOG.info(f"Out of {len(infraenvs)} infraenvs, {data.infraenvs_with_static_network_config} "
              f"were configured with static_network_config")
-    LOG.info(f"Out of {data['infraenvs_with_static_network_config']} were configured with static_network_config,"
-             f" {data['infraenvs_with_static_network_config_bonds']} were configured with bonds")
+    LOG.info(f"Out of {data.infraenvs_with_static_network_config} were configured with static_network_config,"
+             f" {data.infraenvs_with_static_network_config_bonds} were configured with bonds")
--- src/common/stats.py
+++ src/common/stats.py
@@ -1,3 +1,8 @@
+class Dict2Class(object):
+
+    def __init__(self, my_dict):
+        for key in my_dict:
+            setattr(self, key, my_dict[key])
 
 
 def count_unbounded_infraenvs(infraenvs):
@@ -25,10 +30,20 @@ def count_infraenvs_with_static_network_config_bonds(infraenvs):
     return infraenvs_with_static_network_config_bonds
 
 
+def get_cluster_states(clusters):
+    cluster_states = dict()
+    for _, cluster in clusters.items():
+        try:
+            cluster_states[cluster.get('status')] += 1
+        except KeyError:
+            cluster_states[cluster.get('status')] = 1
+    return cluster_states
+
+
 def prepare_data(clusters, infraenvs, hosts):
     data = dict()
+    data['cluster_states'] = get_cluster_states(clusters)
     data['count_unbound_infraenvs'] = count_unbounded_infraenvs(infraenvs)
     data['infraenvs_with_static_network_config'] = count_infraenvs_with_static_network_config(infraenvs)
     data['infraenvs_with_static_network_config_bonds'] = count_infraenvs_with_static_network_config_bonds(infraenvs)
-
-    return data
+    return Dict2Class(data)
